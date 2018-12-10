232c57fea86c5080f6ce6e881f86f3c7e70b01af
ahayn002 <45753476+ahayn002@users.noreply.github.com>
2018-12-10 02:56:24 -0600

    First commit

---
+++ Pages/Statistics.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS312CustomWebsite.Pages
+{
+    public class StatisticsModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Looking at the Numbers...";
+        }
+    }
+}
